8a6ec0287922706b2359fe082b59a00f57e65d52
IInspectable <github@smjh.de>
2020-09-03 13:58:02 +0200

    SchanzenBerechnung + Tests Wurf 1

---
+++ Berechnung/SchanzenBerechnung.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Berechnung {
+
+    public class SchanzenBerechnung {
+
+        SchanzenBerechnung(double länge, double höhe, double absprungwinkelRad, double radius) {
+            Länge             = länge;
+            Höhe              = höhe;
+            AbsprungwinkelRad = absprungwinkelRad;
+            Radius            = radius;
+
+        }
+
+        public double Länge             { get; }
+        public double Höhe              { get; }
+        public double AbsprungwinkelRad { get; }
+        public double AbsprungwinkelDeg => AbsprungwinkelRad * 180 / Math.PI;
+        public double Radius            { get; }
+
+        public static SchanzenBerechnung Berechne(double höhe, double absprungwinkel) {
+
+            var radius = höhe   / (1 - Math.Cos(absprungwinkel));
+            var länge  = radius * Math.Sin(absprungwinkel);
+
+            return new SchanzenBerechnung(
+                länge: länge,
+                höhe: höhe,
+                absprungwinkelRad: absprungwinkel,
+                radius: radius
+            );
+        }
+
+    }
+
+}
---
+++ BerechnungTests/SchanzenBerechnungTests.cs
@@ -0,0 +1,30 @@
+﻿using Berechnung;
+
+using NUnit.Framework;
+
+using System;
+
+namespace BerechnungTests {
+
+    [TestFixture]
+    public class SchanzenBerechnungTests {
+
+        [Test]
+        public void Test1() {
+
+            var winkel     = 19;
+            var rad        = winkel * Math.PI / 180;
+            var höhe       = 0.16;
+            var berechnung = SchanzenBerechnung.Berechne(höhe, winkel * Math.PI / 180);
+
+            Assert.That(berechnung,                   Is.Not.Null);
+            Assert.That(berechnung.Höhe,              Is.EqualTo(höhe));
+            Assert.That(berechnung.AbsprungwinkelRad, Is.EqualTo(rad));
+            Assert.That(berechnung.AbsprungwinkelDeg, Is.EqualTo(winkel));
+            Assert.That(berechnung.Länge,             Is.EqualTo(0.956).Within(0.001));
+            Assert.That(berechnung.Radius,            Is.EqualTo(2.936).Within(0.001));
+        }
+
+    }
+
+}
